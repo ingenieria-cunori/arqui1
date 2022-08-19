fb243558be22c821185bdfef8f5e6aca2fea68d7
heroca60 <heroca60@gmail.com>
2022-08-18 20:25:51 -0600

    Operaciones aritméticas básicas

--- practica03.asm
+++ practica03.asm
@@ -5,74 +5,102 @@
 org 100h        
 
 ;Direccionamiento directo de memoria de programa      
-    ;MOV AX,2020h
-    ;JMP [0101h]
+    MOV AX,2020h
+    JMP [0101h]
     
 ;Direccionamiento relativo de memoria de programa
-    ;MOV AX,2020h
-    ;JMP [2]
+    MOV AX,2020h
+    JMP [2]
 
 ;Direccionamiento indirecto de memoria de programa
-    ;MOV AX, offset var
-    ;JMP AX 
-    ;JMP [2]   
+    MOV AX, offset var
+    JMP AX 
+    JMP [2]   
     
-    ;MOV AX, 0xAABB
-    ;JMP 0106H ;inst
-    ;MOV DX, 1111h   
-;inst:  ;Etiqueta    
-    ;MOV CX, 2222h    
+    MOV AX, 0xAABB
+    JMP 0106H ;inst
+    MOV DX, 1111h   
+inst:  ;Etiqueta    
+    MOV CX, 2222h    
     
     
-    ;MOV AX, 0xAABB
-    ;CALL inst
-    ;MOV DX, 1111h   
-;inst:  ;Etiqueta    
-    ;MOV CX, 2222h  
+    MOV AX, 0xAABB
+    CALL inst2
+    MOV DX, 1111h   
+inst2:  ;Etiqueta    
+    MOV CX, 2222h  
     
           
 ;Etiquetas con JMP    
-    ;MOV AX,00
-    ;MOV BX,00
-    ;MOV CX,01
-    
+    MOV AX,00
+    MOV BX,00
+    MOV CX,01
+     
+    ; Bucle infinito 
     ;L20: ;etiqueta
     ;ADD AX,01
     ;ADD BX,AX
     ;JMP L20
 
-    ;MOV CX,5
-    ;MOV DX,10
-;ciclo:
-    ;INC DX
-    ;DEC CX
-    ;JNZ ciclo
     
     
 ;Instrucciones aritmeticas
-    ;MOV BX,offset var
-    ;ADD AL,BL   ; AL = AL + BL
-    ;ADD CL,44h  ; CL = CL + 44h  
-    ;ADD [BX],02h
+    ;suma de registros
+    MOV AX,05h
+    MOV BX,05h
+    ADD AX,BX ;     equivalente AX = AX + BX       AX+=BX   
     
-    ;Incrementos
-    ;INC BL
-    ;INC BX
+    ;suma inmediata
+    ADD AX,01h
     
-    ;12ab1172h
-    MOV AX,12abh
-    MOV BX,1172h
+    ;Suma de memoria a registro
     
-    ;00000001h
-    MOV CX,0000h
-    MOV DX,0001h
-    ;MOV DX, 0xF000
+    MOV DI,offset var2
+    MOV AL,0
+    ADD AL,[DI]
+    ADD AL,[DI + 1]
+             
+    ;MOV BX, offset var
+    ;ADD AX, var2
+    ;MOV CX,[BX]
+    ;ADD AL,[BX]
     
-    ADD BX, DX
-    ADC AX, CX 
+    
+    ;Suma de incremento
+    INC AX   ; equivalente AX = AX + 1      AX+= 1
+    
+    
+    ;Suma con acarreo
+    MOV AX,0xFFFF
+    MOV CX,0xBCFF
+    MOV BX,0xFFFF
+    MOV DX,0xFFAB
+    ADD AX,CX
+    ADC BX,DX  
+    
+    
+    ;Resta entre registros
+    SUB BX,CX   ; BX = BX - CX
+                          
+    ;resta directa
+    SUB BX,24h
+    
+    
+    ;resta con decremento
+    DEC BX      ; BX = BX - 1       BX-=1                      
+    
+    ;Resta con acarreo
+    MOV AX,0xFFFF
+    MOV CX,0xBCFF
+    MOV BX,0xFFFF
+    MOV DX,0xFFAB
+    SUB AX,CX
+    SBB BX,DX ; considerando el acarreo previo
 
 ret
    var DB 25h
+   var2 DW 10b    ;dimensión de 8 bits  DB
+   ;var2 DW 10b   ;word (palabra) 16bits
 end
 
 
